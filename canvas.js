bb239b3ec1092beb39069bb65be2f7a0ae54dcaf
SejinOH <39869184+SejinO@users.noreply.github.com>
2020-02-28 00:50:47 +0900

    upload

--- canvas.js
+++ canvas.js
@@ -1,47 +1,10 @@
 var canvas = document.querySelector('canvas');
-//canvas라는 변수를 선택자로 만들기
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-//html에서 이 캔버스의 윈도우 설정값을 받아오는 코드
-//창사이즈에 따른 값 변화
-
 var c = canvas.getContext('2d');
 
 c.imageSmoothingEnabled = false;
-//c.fillStyle = 'rgba(255,0,0,0.5)';
-//c.fillRect(100, 100, 100, 100);
-//c.fillStyle = 'rgba(0,255,0,0.5)';
-//c.fillRect(400, 100, 100, 100);
-//c.fillStyle = 'rgba(0,0,255,0.5)';
-//c.fillRect(300, 300, 100, 100);
-//
 console.log(canvas);
-//콘솔에 캔버스 속성 표시
-//
-////Line
-//c.beginPath();
-//c.moveTo(50, 300);
-//c.lineTo(300, 100);
-//c.lineTo(400, 300);
-//c.strokeStyle = 'rgba(100,200,100,255)';
-//c.stroke();
-//
-////Arc / Circle
-////c.beginPath();
-////c.arc(300, 300, 30, 0, Math.PI * 2, false);
-////c.strokeStyle = 'blue';
-////c.stroke();
-//
-//for (var i = 0; i < 100; i++) {
-//	var x  = Math.random()*window.innerWidth;
-//	var y = Math.random()*window.innerHeight;
-//	c.beginPath();
-//	c.arc(x, y, 30, 0, Math.PI * 2, false);
-//	c.strokeStyle = 'blue';
-//	c.stroke();
-//}
-
-
 function Circle(x, y, dx, dy, radius) {
 	this.x = x;
 	this.y = y;
